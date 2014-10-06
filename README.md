1f18ec746ef0b41f11b1f3d4dafb1e2380f8847a
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-06 19:08:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# xdg-trashdir [![Build Status](http://img.shields.io/travis/kevva/xdg-trashdir.svg?style=flat)](https://travis-ci.org/kevva/xdg-trashdir)
+
+> Get the correct trash path on Linux according to the [spec](http://www.ramendik.ru/docs/trashspec.html)
+
+## Install
+
+```sh
+$ npm install --save xdg-trashdir
+```
+
+## Usage
+
+```js
+var trashdir = require('xdg-trashdir');
+
+trashdir(function (err, dir) {
+	if (err) {
+		throw err;
+	}
+
+	console.log(dir);
+	//=> ~/.local/share/Trash
+});
+
+trashdir('foo.zip', function (err, dir) {
+	if (err) {
+		throw err;
+	}
+
+	console.log(dir);
+	//=> /media/johndoe/UUI/.Trash/1000
+});
+```
+
+## License
+
+MIT © [Kevin Mårtensson](https://github.com/kevva)
---
+++ index.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var base = require('xdg-basedir');
+var fs = require('fs');
+var mkdir = require('mkdirp');
+var mount = require('mount-point');
+var path = require('path');
+
+/**
+ * Get the correct trash path on Linux
+ *
+ * @param {String} file
+ * @param {Function} cb
+ * @api public
+ */
+
+module.exports = function (file, cb) {
+	if (typeof file === 'function' && !cb) {
+		cb = file;
+		file = undefined;
+	}
+
+	if (!file) {
+		cb(null, path.join(base.data, 'Trash'));
+		return;
+	}
+
+	mount(file, function (err, res) {
+		if (err) {
+			cb(err);
+			return;
+		}
+
+		if (res.mount === '/') {
+			cb(null, path.join(base.data, 'Trash'));
+			return;
+		}
+
+		var top = path.join(res.mount, '.Trash');
+		var topuid = top + '-' + process.getuid();
+
+		fs.exists(top, function (exists) {
+			if (exists) {
+				fs.lstat(top, function (err, stats) {
+					if (err) {
+						cb(err);
+						return;
+					}
+
+					if (stats.isSymbolicLink()) {
+						mkdir(topuid, function (err) {
+							if (err) {
+								cb(err);
+								return;
+							}
+
+							cb(null, topuid);
+							return;
+						});
+					}
+
+					mkdir(path.join(top, String(process.getuid())), function (err) {
+						if (err) {
+							cb(err);
+							return;
+						}
+
+						cb(null, path.join(top, String(process.getuid())));
+						return;
+					});
+				});
+			}
+
+			mkdir(topuid, function (err) {
+				if (err) {
+					cb(err);
+					return;
+				}
+
+				cb(null, topuid);
+			});
+		});
+	});
+};
