805bbe2075af433efd603aa53fe314cf956bc1a7
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-03 16:59:01 +0100

    Get the home partition
    Fixes #1.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var base = require('xdg-basedir');
 var fs = require('fs');
 var mount = require('mount-point');
 var path = require('path');
+var userHome = require('user-home');
 
 /**
  * Get the correct trash path on Linux
@@ -28,40 +29,47 @@ module.exports = function (file, cb) {
 		return;
 	}
 
-	mount(file, function (err, res) {
+	mount(userHome, function (err, ret) {
 		if (err) {
 			cb(err);
 			return;
 		}
 
-		if (res.mount === '/' || res.mount === '/home') {
-			cb(null, path.join(base.data, 'Trash'));
-			return;
-		}
-
-		var top = path.join(res.mount, '.Trash');
-		var topuid = top + '-' + process.getuid();
-		var stickyBitMode = 17407;
-
-		fs.exists(top, function (exists) {
-			if (!exists) {
-				cb(null, topuid);
+		mount(file, function (err, res) {
+			if (err) {
+				cb(err);
 				return;
 			}
 
-			fs.lstat(top, function (err, stats) {
-				if (err) {
-					cb(null, path.join(base.data, 'Trash'));
-					return;
-				}
+			if (res.mount === ret.mount) {
+				cb(null, path.join(base.data, 'Trash'));
+				return;
+			}
 
-				if (stats.isSymbolicLink() || stats.mode !== stickyBitMode) {
+			var top = path.join(res.mount, '.Trash');
+			var topuid = top + '-' + process.getuid();
+			var stickyBitMode = 17407;
+
+			fs.exists(top, function (exists) {
+				if (!exists) {
 					cb(null, topuid);
 					return;
 				}
 
-				cb(null, path.join(top, String(process.getuid())));
+				fs.lstat(top, function (err, stats) {
+					if (err) {
+						cb(null, path.join(base.data, 'Trash'));
+						return;
+					}
+
+					if (stats.isSymbolicLink() || stats.mode !== stickyBitMode) {
+						cb(null, topuid);
+						return;
+					}
+
+					cb(null, path.join(top, String(process.getuid())));
+				});
 			});
 		});
-	});
+	})
 };
