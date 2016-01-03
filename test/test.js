8c45db18bec7fa106011d4e1ac5fc5616924dc5a
Kevin Martensson <kevinmartensson@gmail.com>
2016-01-03 17:55:11 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -13,18 +13,18 @@ test('get the trash path', async t => {
 });
 
 test('get the trash path using a file', async t => {
-	const dir = await trashdir('index.js');
+	const dir = await trashdir(path.join(__dirname, '../index.js'));
 	t.is(path.join(xdgBasedir.data, 'Trash'), dir);
 });
 
 if (!process.env.TRAVIS) {
 	test('get the trash path on a mounted drive', async t => {
 		const name = 'test-disk';
-		const dirname = path.join(__dirname, '..', name, '.Trash-') + process.getuid();
+		const dirname = path.join(__dirname, name, '.Trash-') + process.getuid();
 
 		await exec(path.join(__dirname, 'mount_create'), [name]);
 
-		const dir = await trashdir(path.join(__dirname, '..', name));
+		const dir = await trashdir(path.join(__dirname, name));
 		t.is(dirname, dir);
 
 		await exec(path.join(__dirname, 'mount_clean'), [name]);
@@ -32,11 +32,11 @@ if (!process.env.TRAVIS) {
 
 	test('get the trash path on a mounted drive with a top trash', async t => {
 		const name = 'test-disk-top';
-		const dirname = path.join(__dirname, '..', name, '.Trash', String(process.getuid()));
+		const dirname = path.join(__dirname, name, '.Trash', String(process.getuid()));
 
 		await exec(path.join(__dirname, 'mount_create'), [name, '--with-trash']);
 
-		const dir = await trashdir(path.join(__dirname, '..', name));
+		const dir = await trashdir(path.join(__dirname, name));
 		t.is(dirname, dir);
 
 		await exec(path.join(__dirname, 'mount_clean'), [name]);
