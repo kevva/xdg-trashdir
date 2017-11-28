2102d352c5dd983bbe07b6e7b83f71135ebc3c7e
Jordan Danford <jordandanford@gmail.com>
2017-11-28 15:29:14 -0700

    Handle dangling symlinks

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from 'path';
 import test from 'ava';
 import xdgBasedir from 'xdg-basedir';
@@ -19,26 +20,36 @@ test('get the trash path using a file', async t => {
 	t.is(path.join(xdgBasedir.data, 'Trash'), dir);
 });
 
+test('get the trash path using a dangling symlink', async t => {
+	const filename = path.join(__dirname, 'dangly');
+	fs.symlinkSync('nonexistent', filename);
+
+	const dir = await trashdir(filename);
+	t.is(path.join(xdgBasedir.data, 'Trash'), dir);
+
+	fs.unlinkSync(filename);
+});
+
 if (!process.env.CI) {
 	test('get the trash path on a mounted drive', async t => {
-		const name = 'test-disk';
-		const dirname = path.join(__dirname, name, '.Trash-') + process.getuid();
+		const name = path.join(__dirname, 'test-disk');
+		const dirname = path.join(name, '.Trash-') + process.getuid();
 
 		await execa(path.join(__dirname, 'mount_create'), [name]);
 
-		const dir = await trashdir(path.join(__dirname, name));
+		const dir = await trashdir(name);
 		t.is(dirname, dir);
 
 		await execa(path.join(__dirname, 'mount_clean'), [name]);
 	});
 
 	test('get the trash path on a mounted drive with a top trash', async t => {
-		const name = 'test-disk-top';
-		const dirname = path.join(__dirname, name, '.Trash', String(process.getuid()));
+		const name = path.join(__dirname, 'test-disk-top');
+		const dirname = path.join(name, '.Trash', String(process.getuid()));
 
 		await execa(path.join(__dirname, 'mount_create'), [name, '--with-trash']);
 
-		const dir = await trashdir(path.join(__dirname, name));
+		const dir = await trashdir(name);
 		t.is(dirname, dir);
 
 		await execa(path.join(__dirname, 'mount_clean'), [name]);
